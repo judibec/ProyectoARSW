8b9902e0db2802db1669d598785cbfd66c7ed114
ser0710 <serroz2822@gmail.com>
2022-12-04 22:33:09 -0500

    arreglo login y autorización

--- src/main/resources/static/js/registry.js
+++ src/main/resources/static/js/registry.js
@@ -3,13 +3,21 @@ async function signIn(){
         auth: {
             clientId: '4d509855-b16c-4287-b8a3-72448421a496',
             authority: 'https://login.microsoftonline.com/common/',
-            redirectUri: 'https://questik-api-1669770264047.azurewebsites.net/start.html'
+            redirectUri: 'http://localhost:8080'
         }
     };
-    var client = new Msal.UserAgentApplication(config);
+    
     var request = {
         scopes: ['user.read']
     };
+
+    var client = new Msal.UserAgentApplication(config);
+
     let loginResponse = await client.loginPopup(request);
     console.dir(loginResponse);
+    var admin_name = loginResponse.account.name;
+    console.dir(loginResponse.account.name);
+    localStorage.setItem("admin_u", admin_name);
+    window.location="start.html"
+    
 }
